97cf925a58071157530c3e242acc62b87174410e
HeitorSichin <heitor.h.sichin@gmail.com>
2019-04-10 00:24:55 -0300

    Estrutura back-end

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
+
+mongoose.connect('mongodb://heitor:heitor123@ds151805.mlab.com:51805/rocketbox', {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //para enviar arquivos nas requisições
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
